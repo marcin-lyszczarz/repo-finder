2ead5e6c9a2f74adfc5be84f6067360916465d4d
Marcin Łyszczarz <lyszczarz.marcin@gmail.com>
2020-12-01 00:21:50 +0100

    add fetch

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -6,4 +6,26 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const btnSearch = document.querySelector(".button");
+
+btnSearch.addEventListener("click", (e) => {
+  e.preventDefault();
+  const searchText = document.querySelector(".search-txt");
+  fetch(`https://api.github.com/users/${searchText.value}/repos`)
+    .then((resp) => resp.json())
+    .then((resp) => {
+      for (let repo of resp) {
+        const { name, html_url } = repo;
+        const {avatar_url} = repo.owner;
+        const resultList = document.querySelector(".result__list--js");
+        const avatar = document.querySelector('.result__img--js');
+        const img = `<img src="${avatar_url}">`;
+        const template = `<li>${name} <a href="${html_url}" title="github prpository link"> GitHub link</a></li>`;
+        avatar.innerHTML = img;
+        resultList.innerHTML += template;
+      }
+    })
+    .catch((error) => {
+      console.log("błąd pobierania");
+    });
+});
